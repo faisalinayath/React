35b99dc1faf1d0cc2c45548dbd43fe626c40da42
Faisal Inayath <faisalinayath16@gmail.com>
2024-08-06 01:52:47 +0530

    Initial commit

--- src/components/product.js
+++ src/components/product.js
@@ -4,7 +4,27 @@ export function Product(){
     return(
         <div class="container my-5">
             <h2 className="text-center mb-3">Product Page</h2>
+            <ProductList/>
+            <ProductForm/>
         </div>
     );
 }
 
+function ProductList(){
+    return (
+        <>
+            <h2 className="text-center mb-3">List Of Products</h2>
+            <button type="button" className="btn btn-primary me-2">Create</button>
+        </>
+    );
+}
+
+
+function ProductForm(){
+    return (
+        <>
+            <h2 className="text-center mb-3">Create A New Product</h2>
+            <button type="button" className="btn btn-danger me-2">Cancel</button>
+        </>
+    );
+}
